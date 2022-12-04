030184e315a0c09bd5d2ead506f3f39cb23e80a2
NChornenka <88328438+NChornenka@users.noreply.github.com>
2022-12-03 20:04:44 -0500

    Added countdown

---
+++ index.js
@@ -0,0 +1,33 @@
+function christmasCountdown (){
+    const christmasDate = new Date("December 25, 2022 00:00");
+    const now = new Date();
+    const difference = christmasDate - now;
+
+    const msInS = 1000;
+    const msInM = msInS * 60;
+    const msInH = msInM * 60;
+    const msInD = msInH * 24;
+
+    let days = Math.floor(difference/msInD);
+    let hours = Math.floor((difference % msInD)/msInH);
+    let minutes = Math.floor((difference%msInH) / msInM);
+    let seconds = Math.floor((difference%msInM) / msInS);
+
+    document.querySelector("#day").textContent = days;
+    document.querySelector("#hour").textContent = hours;
+    document.querySelector("#minute").textContent = minutes;
+    document.querySelector("#second").textContent = seconds;
+
+    if (difference <=0 ){
+        document.querySelector("#day").textContent = 0;
+        document.querySelector("#hour").textContent = 0;
+        document.querySelector("#minute").textContent = 0;
+        document.querySelector("#second").textContent = 0;
+
+        clearInterval(timer);
+    }
+
+    
+}
+
+let timer = setInterval(christmasCountdown, 1000);
